ce000e1ecdf8542cef8073e281ab152b8a4d492d
Greg Miller <w4ik@RedMenaceUbuntu.(none)>
2012-11-25 10:23:39 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Benwmiller::Application.config.secret_token = 'aa75f1db8d2f3a8ac7ee5b70cdd1c3e5ff8f0e0fa5137ec6acc7dfc339f9e15980581acd11eb0a2a51b18f7c71d189996feb091de7cc5f9ca57c9d76b1b6d841'
