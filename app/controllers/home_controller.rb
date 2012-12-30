0a29015ed480fd5072ab80e8cf041e59e6f4d568
Greg Miller <w4ik@arrl.net>
2012-12-29 20:24:33 -0500

    added pokedex in lib

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
+  require './lib/pokedex'
+
+
   def index
   end
 end
---
+++ lib/pokedex.rb
@@ -0,0 +1,12 @@
+
+# Greg Miller - Canoe Creek Software - 12/29/2012
+# benwmiller.com - pokemon interactive index
+# Used to build and retrieve from a pokedex
+#
+class Pokedex
+
+  def test_pokedex
+    return "hello from Pikachu"
+  end   
+
+end
